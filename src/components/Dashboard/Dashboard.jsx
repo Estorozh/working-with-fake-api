5c1f04c4e1b6c3951869e0c2c0866af0a5665e51
EStorozh <mr.ahilis@gmail.com>
2020-09-05 16:53:57 +0300

    display any data from fake-api

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,31 @@
-import React, {useEffect} from 'react'
-import { useDispatch } from 'react-redux'
-import { setContacts } from './dashboardSlice.js'
+import React, {useEffect, useState} from 'react'
+import { useDispatch, useStore } from 'react-redux'
+import { setContacts, fetching } from './dashboardSlice.js'
 
 const Dashboard = () => {
     const dispatch = useDispatch()
     const user = sessionStorage.getItem('auth') || localStorage.getItem('auth')
+    let store = useStore()
+    const [isFetching, setFetching] = useState(getStatus())
+    
+    function getStatus() {
+        return store.getState().dashboard.isFetching
+    }
+    function displayContacts() {
+        return Object.values(store.getState().dashboard.contacts).map(arr => {
+            return arr.map(Obj => (<p key={Obj.id}>{Obj.phone}</p> ) )
+        })
+    }
+
     useEffect(()=> {
+        dispatch(fetching())
+        setFetching(getStatus())
         async function getContacts() {
             try {
                 let contacts = await fetch('http://localhost:3001/users')
                 contacts = await contacts.json()
                 dispatch(setContacts({contacts}))
+                setFetching(getStatus())
             } catch (err) {
                 // eslint-disable-next-line no-console
                 console.error(err.toString())
@@ -18,8 +33,12 @@ const Dashboard = () => {
         }
         getContacts()
     }, [])
+
     return (<>
         <h1>Hello, {user}</h1>
+        <div>
+            {isFetching ? "Loading..." : displayContacts() }
+        </div>
     </>)
 }
 export default Dashboard
--- src/components/Dashboard/dashboardSlice.js
+++ src/components/Dashboard/dashboardSlice.js
@@ -1,16 +1,9 @@
 import {createSlice} from '@reduxjs/toolkit'
 
-const initialState = {}
-// () => async dispatch => {
-//     // dispatch()
-//     try {
-//         let contacts = await fetch('http://localhost:3001/users')
-//         console.log(contacts.json())
-//         return contacts.json()
-//     } catch(e) {
-//         console.log(e.toString())
-//     }
-// }
+const initialState = {
+    isFetching: false,
+    contacts: []
+}
 
 const dashboardSlice = createSlice({
     name: 'dashboard',
@@ -18,9 +11,13 @@ const dashboardSlice = createSlice({
     reducers: {
         setContacts(state, action) {
             state.contacts = action.payload
+            state.isFetching = false
+        },
+        fetching(state, action) {
+            state.isFetching = true
         }
     }
 })
 
-export const {setContacts} = dashboardSlice.actions
+export const {setContacts, fetching} = dashboardSlice.actions
 export default dashboardSlice.reducer;
